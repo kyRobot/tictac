d4cc538352dc18b947ba2351a2e6ffd4b24a3e40
kyRobot <kylemilner@me.com>
2019-02-11 14:44:59 +0800

    Enable turn taking

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xNext: true
     };
   } 
 
@@ -30,12 +31,15 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares:squares});
+    squares[i] = this.state.xNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xNext:   !this.state.xNext
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xNext ? 'X' : 'O');
 
     return (
       <div>
